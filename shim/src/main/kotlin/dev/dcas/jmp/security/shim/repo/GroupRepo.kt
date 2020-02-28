9b84ac9915aaa254aaca09974510bce917b2eabd
Django Cass <django@dcas.dev>
2020-02-28 21:23:04 +1100

    Added explicit HQL query for GroupRepo::findAllByUsersIsContaining

--- shim/src/main/kotlin/dev/dcas/jmp/security/shim/repo/GroupRepo.kt
+++ shim/src/main/kotlin/dev/dcas/jmp/security/shim/repo/GroupRepo.kt
@@ -9,6 +9,7 @@ package dev.dcas.jmp.security.shim.repo
 import dev.dcas.jmp.security.shim.entity.Group
 import dev.dcas.jmp.security.shim.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -16,5 +17,7 @@ import java.util.*
 interface GroupRepo: JpaRepository<Group, UUID> {
 	fun findFirstByName(name: String): Group?
 	fun findAllByPublicIsTrue(): List<Group>
+
+	@Query("FROM Group g WHERE :user MEMBER g.users")
 	fun findAllByUsersIsContaining(user: User): List<Group>
 }
